2be4e09686b62b46f47ad70083739f32c76255dc
xin522758754 <xin522758754@qq.com>
2019-10-29 17:37:44 +0800

    render pipeline

--- Assets/Scripts/RenderPipeline/RenderPipeline.cs
+++ Assets/Scripts/RenderPipeline/RenderPipeline.cs
@@ -38,6 +38,20 @@ public class RenderPipeline
             RenderTexture.ReleaseTemporary(sceneCameraRt);
             sceneCameraRt = null;
         }
+
+        //RT是一张特殊的texture，可以real-time将camera拍摄的画面写入texture。
+        //实现：将FBO直接关联一个gpu上的texture对象，这样就等于在绘制时就直接绘制到这个texure上，这样也省去了拷贝时间，gles中一般是使用FramebufferTexture2D（）这样的接口。
+        //RT作为"native engine object" 类型，不会被GC释放，所以留心RT的生存时间，及时手动释放RT就非常重要了。
+        //如果用blit做一系列的后处理，最高效的方法是在每一次blit的时候都获取和释放RT（unity有一个RT pool）。而不是提前声明一个RT并且反复使用。这对于移动平台尤其有益。
+        //可以使用camera的特性比如mask;整个屏幕的后处理post-processing,Unity提供方便接口直接把需要渲染的画面写入RT。高效：因为不用拷贝操作就可以获得RT，并且对texture处理后直接输出在屏幕。
+        //
+
+        //GrabPass  Unity shader中内置的特殊pass，可以实现与RT类似功能，但原理、用法和效率有不同。
+        //把当前整个屏幕的back buffer复制写入一张texture，（并不包括当前shader正在渲染的物体），这张texture可以被用在接下来的pass中做一些处理。
+        //如果是被命名的texture，同样可以用在其他shader中。通常用作处理屏幕部分区域特效，比如扭曲火焰，毛玻璃。,效率不如RT，但使用方便，不用手动控制grab时机。
+
+
+
         sceneCameraRt = RenderTexture.GetTemporary(
             1136, 640, //此处可以扩展成动态分辨率 根据机型高中低
             16, //此处可以扩展成动态深度 根据机型高中低
@@ -49,6 +63,9 @@ public class RenderPipeline
 
         //newRt.filterMode = FilterMode.Point;
         sceneCameraRt.name = "RenderPipelineRT";
+
+        //待实现
+        //需要把RT关联到每一个相机上
     }
 
     //2.在摄像机开始渲染场景之前调用,按照渲染的优先顺序调用（unity 先调用depth小的相机）
